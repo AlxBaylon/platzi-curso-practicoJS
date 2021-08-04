1aa75949b81de6a4d725c4dca43de948e9ccd73c
alxbaylon <alxbay97@gmail.com>
2021-08-03 21:01:32 -0500

    Making functions

--- figurasGeo/figuras.js
+++ figurasGeo/figuras.js
@@ -1,45 +1,61 @@
 // -----Cuadrados ------------------------------------------
 console.group("Cuadrados");
-const ladoCuad = 5;
-console.log(`El lado del cuadrado es: ${ladoCuad} cm`);
+// const ladoCuad = 5;
+// console.log(`El lado del cuadrado es: ${ladoCuad} cm`);
 
-const periCuad = ladoCuad * 4;
-console.log(`El perímetro del cuadrado es: ${periCuad} cm`);
+function CalcularPeriCuad (ladoCuad){
+	const periCuad = ladoCuad * 4;
+	console.log(`El perímetro del cuadrado es: ${periCuad} cm`);
+}
 
-const areaCuad = ladoCuad**2;
-console.log(`El área del cuadrado es: ${areaCuad} cm2`);
+function CalcularAreaCuad (ladoCuad){
+	const areaCuad = ladoCuad**2;
+	console.log(`El área del cuadrado es: ${areaCuad} cm2`);
+}
 console.groupEnd();
 
 
 // -----Triángulos------------------------------------------
 console.group("Triángulos");
-const ladoTri1 = 6;
-const ladoTri2 = 6;
-const baseTri = 4;
-const alturaTri = 5.5;
-console.log(`Los lados del triángulo son: ${ladoTri1} cm, ${ladoTri2} cm`);
-console.log(`La base del triángulo es: ${baseTri} cm`);
-console.log(`La altura del triángulo es: ${alturaTri} cm`);
+// const ladoTri1 = 6;
+// const ladoTri2 = 6;
+// const baseTri = 4;
+// const alturaTri = 5.5;
+// console.log(`Los lados del triángulo son: ${ladoTri1} cm, ${ladoTri2} cm`);
+// console.log(`La base del triángulo es: ${baseTri} cm`);
+// console.log(`La altura del triángulo es: ${alturaTri} cm`);
 
-const periTri = ladoTri1 + ladoTri2 + baseTri;
-console.log(`El perímetro del triángulo es: ${periTri} cm`);
+function CalcularPeriTri(ladoTri1, ladoTri2, baseTri){
+	const periTri = ladoTri1 + ladoTri2 + baseTri;
+	console.log(`El perímetro del triángulo es: ${periTri} cm`);
+}
 
-const areaTri = (baseTri * alturaTri)/2;
-console.log(`El área del triángulo es: ${areaTri} cm2`);
+function CalcularAreaTri(baseTri, alturaTri){
+	const areaTri = (baseTri * alturaTri)/2;
+	console.log(`El área del triángulo es: ${areaTri} cm2`);
+}
 console.groupEnd();
 
 
 // -----Círculos--------------------------------------------
 console.group("Círculos");
-const radioCir = 5;
-const diamCir = radioCir * 2;
-const PI = Math.PI;
-console.log(`El radio del círculo es: ${radioCir} cm`);
-console.log(`El diámetro del círculo es: ${diamCir} cm`);
+// const radioCir = 5;
+// const diamCir = radioCir * 2;
+// const PI = Math.PI;
+// console.log(`El radio del círculo es: ${radioCir} cm`);
+// console.log(`El diámetro del círculo es: ${diamCir} cm`);
 
-const periCir = diamCir * PI;
-console.log(`El perímetro del triángulo es: ${periCir.toFixed(2)} cm`);
+function CalcularDiametro(radio){
+	return radio*2;
+}
 
-const areaCir = (radioCir**2) * PI;
-console.log(`El perímetro del triángulo es: ${areaCir.toFixed(2)} cm`);
+function CalcularPeriCir(radioCir){
+	const periCir = CalcularDiametro(radioCir) * Math.PI;
+	console.log(`El perímetro del Círculo es: ${periCir.toFixed(2)} cm`);
+}
+
+function CalcularAreaCir(radioCir){
+	const areaCir = (radioCir**2) * Math.PI;
+	console.log(`El área del círculo es: ${areaCir.toFixed(2)} cm2`);
+}
 console.groupEnd();
