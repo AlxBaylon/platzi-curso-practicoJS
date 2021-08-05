7efe67c0d6c23faf0fe05a808436b896937c185d
alxbaylon <alxbay97@gmail.com>
2021-08-05 15:55:09 -0500

    Discounts - writting HTML from JS

--- discounts/discounts.js
+++ discounts/discounts.js
@@ -7,3 +7,20 @@ function CalcularPrecioConDescuento(precio, descuento){
 
 	return precioConDescuento;
 }
+
+function onClickDiscountPrice(){
+	const inputPrice = document.getElementById("InputPrice");
+	const priceValue = inputPrice.value;
+
+	const inputDiscount = document.getElementById("InputDiscount");
+	const discountValue = inputDiscount.value;
+
+	if(priceValue == 0 || discountValue == 0){
+		alert(`Ingrese valores correctos`);
+	} else {
+		const precioConDescuento = CalcularPrecioConDescuento(priceValue, discountValue);
+
+		const resultP = document.getElementById("ResultP");
+		resultP.innerText = `El precio con descuento es: $${precioConDescuento}`;
+	}
+}
