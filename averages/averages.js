9cd8d86ae0cdf169d780ca51280bb9af0bdbf072
alxbaylon <alxbay97@gmail.com>
2021-08-07 15:42:59 -0500

    Averages - First Class - find method

--- averages/averages.js
+++ averages/averages.js
@@ -1,10 +1,16 @@
 const lista = [1,2,4,5,7,8];
 
 function CalcularMediaAritmetica(lista) {
-	let sumaLista = 0;
-	for(let i=0; i < lista.length; i++){
-		sumaLista += lista[i]
-	}
+	// let sumaLista = 0;
+	// for(let i=0; i < lista.length; i++){
+	// 	sumaLista += lista[i]
+	// }
+
+	const sumaLista = lista.reduce(
+		function (valorAcumulado, nuevoElemento) {
+			return valorAcumulado + nuevoElemento
+		}, 0
+	);
 
 	const promedioLista = sumaLista/lista.length;
 
