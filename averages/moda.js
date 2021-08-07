6e980aba64245bb108b9102c9997dd3e28dda7aa
alxbaylon <alxbay97@gmail.com>
2021-08-07 18:39:37 -0500

    Fixed modes

--- averages/moda.js
+++ averages/moda.js
@@ -1,18 +1,21 @@
-const lista1 = [1,2,3,1,2,3,4,2,2,2,1,2];
+const lista1 = [1,2,3,1,3,4,2,1,2];
 
-const lista1Count = {};
+const CalcularModa = (lista) => {
+	const listaCount = {};
 
-lista1.map(function (elemento) {
-	if(lista1Count[elemento]){
-		lista1Count[elemento] += 1;
-	} else{
-		lista1Count[elemento] = 1;
+	lista.map(function (elemento) {
+		if(listaCount[elemento]){
+			listaCount[elemento] += 1;
+		} else{
+			listaCount[elemento] = 1;
+		}
+	});
+
+	const listaArray = Object.entries(listaCount).sort((elementoA, elementoB) => {
+		return elementoA[1] - elementoB[1];
 	}
-});
-
-const lista1Array = Object.entries(lista1Count).sort((elementoA, elementoB) => {
-	return elementoA[1] - elementoB[1];
+	);
+	const	moda = listaArray[listaArray.length - 1];
+	
+	return moda;
 }
-);
-
-const moda = lista1Array[lista1Array.length - 1];
